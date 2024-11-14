eedba2587afa0bc94480eb1ac78a5b1c06f77f8e
sotiristiga <99202968+sotiristiga@users.noreply.github.com>
2024-11-14 14:39:50 +0200

    Create image_streamlit.py

---
+++ python code/image_streamlit.py
@@ -0,0 +1,5 @@
+def download_image(url, save_as):
+    urllib.request.urlretrieve(url, save_as)
+
+download_image('https://raw.githubusercontent.com/sotiristiga/Euroleague_dash/refs/heads/main/eurologo.png','eurologo.png')
+st.image(Image.open("eurologo.png"),width=100)
